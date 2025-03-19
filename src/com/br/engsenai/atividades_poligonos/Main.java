a79e2d8811027e4b9ad40a4ef70b53a8d7dca548
CARLOS EDUARDO SILVA PEREIRA <25132872@dsn127m1249386.cfp127aluno.local>
2025-03-19 17:02:58 -0300

    Trapézio classe concluído

--- src/com/br/engsenai/atividades_poligonos/Main.java
+++ src/com/br/engsenai/atividades_poligonos/Main.java
@@ -1,15 +1,9 @@
 package com.br.engsenai.atividades_poligonos;
-
-import com.br.engsenai.atividades_poligonos.model.Quadrado;
-
+  
 public class Main {
 
 	public static void main(String[] args) {
 		
-		Quadrado q1 = new Quadrado();
-		q1.setLado(5);
-		q1.mostrarDados();
-
 	}
-
+  
 }
