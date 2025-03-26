1c8da19ea9e28d01496842ed9d357e027ecebf48
CARLOS EDUARDO SILVA PEREIRA <25132872@CFP127ALUNO.LOCAL>
2025-03-26 16:59:17 -0300

    menu iniciado.

--- src/com/br/engsenai/atividades_poligonos/Main.java
+++ src/com/br/engsenai/atividades_poligonos/Main.java
@@ -1,9 +1,23 @@
 package com.br.engsenai.atividades_poligonos;
-  
+
+import java.util.Scanner;
+
+import com.br.engsenai.atividades_poligonos.ui.Menu;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Digite seu nome: ");
+		String nome = scanner.next();
+		
+		System.out.println(nome.length());
+		System.out.println("Olá, " + nome);
+		System.out.println(nome.toUpperCase());
+		
+		Menu menu = new Menu();
+		menu.criarMenu();
 	}
-  
 }
--- src/com/br/engsenai/atividades_poligonos/model/Quadrado.java
+++ src/com/br/engsenai/atividades_poligonos/model/Quadrado.java
@@ -3,5 +3,31 @@ package com.br.engsenai.atividades_poligonos.model;
 public class Quadrado {
 	
 	private double lado;
+	public void setLado(double lado) {
+ 	
+     }
+ 		
+ 	
+ 	private double calcularArea() {
+ 		double area = lado * lado;
+ 		return area;
+ 	}
+      
+ 	private double calcularPerimetro() {
+ 		double perimetro = 4 * lado;
+ 		return perimetro;		
+ 	}
+ 	
+ 	public void mostrarDados() {
+ 		System.out.println("----------------");
+ 		System.out.println("DADOS DO  QUADRADO");
+ 		System.out.println("----------------");
+ 		System.out.println("LADO: " + lado);
+ 		System.out.println("ÁREA: " + + calcularArea());
+ 		System.out.println("PERÍMETRO: " + calcularPerimetro());
+ 		System.out.println("----------------");
+ 		
+ 		
+ 	}
 
 }
